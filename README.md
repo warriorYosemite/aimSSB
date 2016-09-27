5ac082d0df04f5ab0446f416082b6f5cefb30e0d
Shanu Khera <shanu.khera@octro.com>
2016-09-27 19:29:40 +0530

    init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-WEB CODE
+web scraper
--- main_server.py
+++
@@ -1,6 +0,0 @@
-import flask
-app = flask.Flask(__name__)
-@app.route("/")
-def index():
-	return "hello world"
-app.run(host="0.0.0.0", port=4321, debug=True)
