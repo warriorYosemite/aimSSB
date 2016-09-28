aed62b0f759bd21aee0f874606d3de6a2987df26
Shanu Khera <shanu.khera@octro.com>
2016-09-28 16:56:25 +0530

    better

--- get_all_posts.py
+++ get_all_posts.py
@@ -3,17 +3,25 @@ import pprint
 import xml.etree.ElementTree as etree
 from HTMLParser import HTMLParser
 
+def get_val(it, tp):
+    for t in tp:
+        it = it.__getitem__(t)
+    return it
+
 class SSBParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        for t,a in self.to_store:
+        for t,a,n,ln in self.to_store:
             if t == tag and (len(attrs)==0 or a in attrs[0]):
-                self.to_return.append({str(tag):None})
-                self.switch = True
+                if n!=None:
+                    val = get_val(attrs,n)
+                else:
+                    val = str(tag)
+                if len(val) > ln:
+                    self.to_return.append(val)
+                    self.switch = True
     def handle_data(self, data):
         if self.switch:
-            d = self.to_return[-1]
-            d[d.keys()[0]] = data
-            self.to_return[-1] = d
+            self.to_return[-1] = {self.to_return[-1]:data}
             self.switch = not self.switch
     def tag_wrap(self, to_store):
         self.to_store  = to_store
@@ -39,11 +47,18 @@ all_urls = tuple(all_urls)
 ###########################################################
 
 ########################## PARSE ALL URLS AND GET CONTENTS ################################################
+data_to_scrape = (
+                    ("meta",   "description", (1,1), -1),
+                    ("h1",     "entry-title", None,  -1),
+                    ("span",   "EN-IN",       None,   5),
+                    ("p",      "",            None,   5),
+                    ("title",  "",            None,  -1)
+                )
 parser = SSBParser()
 all_data = []
-for url in all_urls:
+for url in all_urls[0:1]:
     html_resp = requests.get(url).text
-    scraped_data = L_scrape(html_resp, (("h1", "entry-title"),("span", "EN-IN"),("p", "")) , url, parser)
+    scraped_data = L_scrape(html_resp, data_to_scrape , url, parser)
     all_data.append(scraped_data)
 ##########################################################################################################
 pprint.pprint(all_data)
